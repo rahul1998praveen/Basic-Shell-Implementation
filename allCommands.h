4a317ea9383a2d4f52b07d21deb0fbaf4f0c4187
rahul praveen <rahulpraveen16@gmail.com>
2020-04-22 00:34:48 +0100

    Create allCommands.h

---
+++ allCommands.h
@@ -0,0 +1,12 @@
+// RAHUL PRAVEEN 
+// 16203022
+// rahul@ucdconnect.ie
+// Header file for all commands
+#ifndef ALLCOMMANDS
+#define ALLCOMMANDS
+#endif //ALLCOMMANDS
+void prompt();
+void ctrlCHandler(int sig);
+void commandCD(char** arguments);
+void userCommands(char* command);
+
